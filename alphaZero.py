a7e29b24888a3fd61b52cc47a075c5afeae4a888
Arbab Husain <arbabhusain200@gmail.com>
2024-12-22 20:37:19 -0500

    Added AlphaZero algorithm

---
+++ alphaZero.py
@@ -0,0 +1,106 @@
+import random
+import numpy as np
+import torch
+from main import TicTacToe
+from mcts import MCTS
+from tqdm import trange
+import torch.nn as nn
+import torch.nn.functional as F
+from nnmodel import ResNet
+
+
+class AlphaZero:
+    def __init__(self, model, optimizer, game, args):
+        self.model = model
+        self.optimizer = optimizer
+        self.game = game
+        self.args = args
+        self.mcts = MCTS(game, args, model)
+        
+    def selfPlay(self):
+        memory = []
+        player = 1
+        state = self.game.get_initial_state()
+        
+        while True:
+            neutral_state = self.game.change_perspective(state, player)
+            action_probs = self.mcts.search(neutral_state)
+            
+            memory.append((neutral_state, action_probs, player))
+            
+            action = np.random.choice(self.game.board, p=action_probs)
+            
+            state = self.game.get_next_state(state, action, player)
+            
+            value, is_terminal = self.game.get_value_and_terminated(state, action)
+            
+            if is_terminal:
+                returnMemory = []
+                for hist_neutral_state, hist_action_probs, hist_player in memory:
+                    hist_outcome = value if hist_player == player else self.game.get_opponent_value(value)
+                    returnMemory.append((
+                        self.game.get_encoded_state(hist_neutral_state),
+                        hist_action_probs,
+                        hist_outcome
+                    ))
+                return returnMemory
+            
+            player = self.game.get_opponent(player)
+                
+            
+    
+    def train(self, memory):
+        random.shuffle(memory)
+        for batchIdx in range(0, len(memory), self.args['batch_size']):
+            sample = memory[batchIdx:min(len(memory) - 1, batchIdx + self.args['batch_size'])] # Change to memory[batchIdx:batchIdx+self.args['batch_size']] in case of an error
+            state, policy_targets, value_targets = zip(*sample)
+            
+            state, policy_targets, value_targets = np.array(state), np.array(policy_targets), np.array(value_targets).reshape(-1, 1)
+            
+            state = torch.tensor(state, dtype=torch.float32)
+            policy_targets = torch.tensor(policy_targets, dtype=torch.float32)
+            value_targets = torch.tensor(value_targets, dtype=torch.float32)
+            
+            out_policy, out_value = self.model(state)
+            
+            policy_loss = F.cross_entropy(out_policy, policy_targets)
+            value_loss = F.mse_loss(out_value, value_targets)
+            loss = policy_loss + value_loss
+            
+            self.optimizer.zero_grad() 
+            loss.backward()
+            self.optimizer.step() 
+    
+    def learn(self):
+        for iteration in range(self.args['num_iterations']):
+            memory = []
+            
+            self.model.eval()
+            for selfPlay_iteration in trange(self.args['num_selfPlay_iterations']):
+                memory += self.selfPlay()
+                
+            self.model.train()
+            for epoch in trange(self.args['num_epochs']):
+                self.train(memory)
+            
+            torch.save(self.model.state_dict(), f"model_{iteration}.pt")
+            torch.save(self.optimizer.state_dict(), f"optimizer_{iteration}.pt")
+
+
+tictactoe = TicTacToe()
+
+model = ResNet(tictactoe, 4, 64)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+args = {
+    'C': 2,
+    'num_searches': 60,
+    'num_iterations': 3,
+    'num_selfPlay_iterations': 50,
+    'num_epochs': 4,
+    'batch_size': 64
+}
+
+alphaZero = AlphaZero(model, optimizer, tictactoe, args)
+alphaZero.learn()
--- nntest.py
+++ nntest.py
@@ -6,8 +6,10 @@ from main import TicTacToe
 tictactoe = TicTacToe()
 
 state = tictactoe.get_initial_state()
-state = tictactoe.get_next_state(state, 2, 1)
-state = tictactoe.get_next_state(state, 7, -1)
+state = tictactoe.get_next_state(state, 2, -1)
+state = tictactoe.get_next_state(state, 4, -1)
+state = tictactoe.get_next_state(state, 8, 1)
+state = tictactoe.get_next_state(state, 6, 1)
 
 print(state)
 
@@ -18,6 +20,8 @@ print(encoded_state)
 tensor_state = torch.tensor(encoded_state).unsqueeze(0)
 
 model = ResNet(tictactoe, 4, 64)
+model.load_state_dict(torch.load('model_2.pt'))
+model.eval()
 
 policy, value = model(tensor_state)
 value = value.item()
